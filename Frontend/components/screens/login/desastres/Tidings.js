db125540960a26bee58175fe2bbeffc88be114a4
lenadroi35s <santanaleandro099@gmail.com>
2023-08-20 21:03:31 -0500

    centrado tidings

--- Frontend/components/screens/login/desastres/Tidings.js
+++ Frontend/components/screens/login/desastres/Tidings.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import {
   Box,
@@ -8,83 +8,130 @@ import {
   NativeBaseProvider,
   VStack,
 } from "native-base";
+import containers from "../../../styles/containers";
+import { Context } from "../../../globalContext/globalContext";
 
 function Tidings({ navigation }) {
-
-  const Celcius = weatherData?.main?.temp - 273.15;
+  const globalContext = useContext(Context);
+  const { isLoggedIn, appSettings } = globalContext;
   return (
     <NativeBaseProvider>
       <Container>
-        <View>
-          <Box>
-            <View>
-              <Center>
+        <View style={containers(appSettings).outerPage}>
+          <Center my={10}>
+            <Image
+              alt="logo"
+              size={150}
+              borderRadius={30}
+              source={{
+                uri: "https://cdn-icons-png.flaticon.com/512/6683/6683592.png",
+              }}
+            />
+          </Center>
+          <VStack space={4} alignItems="center">
+            <TouchableOpacity onPress={() => navigation.navigate("Preparing")}>
+              <Center
+                w="64"
+                h="20"
+                bg="indigo.300"
+                rounded="md"
+                shadow={3}
+                borderWidth={2}
+                borderColor="#2F3370"
+              >
                 <Image
                   alt="logo"
-                  size={150}
-                  borderRadius={30}
+                  style={{
+                    width: 40,
+                    height: 40,
+                    borderRadius: 20,
+                    marginRight: 10,
+                  }}
                   source={{
-                    uri: "https://cdn-icons-png.flaticon.com/512/6683/6683592.png",
+                    uri: "https://i.ibb.co/W22nPQz/preparate-desastre-natural.png",
                   }}
                 />
+                <Text>Prepárate para un desastre natural</Text>
               </Center>
-            </View>
-          </Box>
-       
-          <View>
-            {/* Option 1 */}
-            <TouchableOpacity onPress={() => navigation.navigate("Preparing")}>
-              <Image
-                alt="logo"
-                size={150}
-                borderRadius={30}
-                source={{
-                  uri: "https://i.ibb.co/W22nPQz/preparate-desastre-natural.png",
-                }}
-              />
-              <Text>Prepárate para un desastre natural</Text>
             </TouchableOpacity>
 
-            {/* Option 2 */}
             <TouchableOpacity onPress={() => navigation.navigate("warnings")}>
-              <Image
-                alt="logo"
-                size={150}
-                borderRadius={30}
-                source={{
-                  uri: "https://i.ibb.co/3C9cQ95/alertas-desastres.png",
-                }}
-              />
-              <Text>Alertas de desastres</Text>
-            </TouchableOpacity>
-          </View>
-          <View>
-            {/* Option 3 */}
-            <TouchableOpacity onPress={() => navigation.navigate("view")}>
-              <Image
-                alt="logo"
-                size={150}
-                borderRadius={30}
-                source={{
-                  uri: "https://i.ibb.co/RCNwsqw/ver-desastres-recientes.png",
-                }}
-              />
-              <Text>Ver desastres recientes</Text>
+              <Center
+                w="64"
+                h="20"
+                bg="indigo.500"
+                rounded="md"
+                shadow={3}
+                borderWidth={2}
+                borderColor="#2F3370"
+              >
+                <Image
+                  alt="logo"
+                  style={{
+                    width: 40,
+                    height: 40,
+                    borderRadius: 20,
+                    marginRight: 10,
+                  }}
+                  source={{
+                    uri: "https://i.ibb.co/3C9cQ95/alertas-desastres.png",
+                  }}
+                />
+                <Text>Alertas de desastres</Text>
+              </Center>
             </TouchableOpacity>
 
-            {/* Option 4 */}
-            <TouchableOpacity onPress={() => navigation.navigate("Recovery")}>
-              <Image
-                alt="logo"
-                size={150}
-                borderRadius={30}
-                source={{
-                  uri: "https://i.ibb.co/ncZhwqs/recuperarse-desatres-naturales.png",
-                }}
-              />
-              <Text>Recuperarse de un desastre natural</Text>
+            <TouchableOpacity onPress={() => navigation.navigate("view")}>
+              <Center
+                w="64"
+                h="20"
+                bg="indigo.700"
+                rounded="md"
+                shadow={3}
+                borderWidth={2}
+                borderColor="#2F3370"
+              >
+                <Image
+                  alt="logo"
+                  style={{
+                    width: 40,
+                    height: 40,
+                    borderRadius: 20,
+                    marginRight: 10,
+                  }}
+                  source={{
+                    uri: "https://i.ibb.co/RCNwsqw/ver-desastres-recientes.png",
+                  }}
+                />
+                <Text>Ver desastres recientes</Text>
+              </Center>
             </TouchableOpacity>
-          </View>
+            <TouchableOpacity onPress={() => navigation.navigate("Recovery")}>
+              <Center
+                w="64"
+                h="20"
+                bg="indigo.700"
+                rounded="md"
+                shadow={3}
+                borderWidth={2}
+                borderColor="#2F3370"
+              >
+                <Image
+                  alt="logo"
+                  style={{
+                    width: 40,
+                    height: 40,
+                    borderRadius: 20,
+                    marginRight: 10,
+                  }}
+                  source={{
+                    uri: "https://i.ibb.co/ncZhwqs/recuperarse-desatres-naturales.png",
+                  }}
+                />
+                <Text>Recuperarse de un desastre natural</Text>
+              </Center>
+            </TouchableOpacity>
+          </VStack>
         </View>
       </Container>
     </NativeBaseProvider>
